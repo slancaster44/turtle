75bb25c7ab3fd60c66ee815c452d96a8aa46aba4
Stanley <lancasterharp@gmail.com>
2023-04-17 18:19:10 -0500

    Converted to z80 repo

--- README.md
+++
@@ -1,4 +0,0 @@
-# turtle
-Turtle/LLVM Compiler
-
-Extension on the ludwig programming language, as well as an llvm-based compiler
--- test/test.cpp
+++
@@ -1,9 +0,0 @@
-#include "tokenTests.hpp"
-#include "lexerTests.hpp"
-#include "parserTests.hpp"
-
-int main() {
-    runParserTests();
-    runLexerTests();
-    runTokenTests();
-}
